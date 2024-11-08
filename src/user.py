402b16ea3f3668e4cc429e552390615ca4723453
Nafikova_Julia <nafikova91@yandex.ru>
2024-11-08 10:55:18 +0300

    add modules user and main

--- src/user.py
+++ src/user.py
@@ -1,3 +1,14 @@
-def user():
-    """Функция для взаимодействия с пользователем"""
-    pass
+from src.operations_on_vacancies import OperationsOnVacancies
+
+def salary_range(vacancies, salary_from):
+    """Возвращает список вакансий с заданным значением зарплаты"""
+    return [result for result in vacancies if result.salary >= salary_from]
+
+def sorting_by_salary(vacancies):
+    """Сортирует вакансии по зарплате"""
+    return  sorted(vacancies, key=lambda vacancy: vacancy.salary, reverse=True)
+
+def top_n_salary(vacancies, top_n):
+    """Возвращает топ N вакансий по зарплате"""
+    sort_vacancies = sorting_by_salary(vacancies)
+    return  sort_vacancies[:top_n]
